56e39ff4bb97ab46099ea8a7babae5046f2c2122
SRT <sranjant4@gmail.com>
2024-02-08 15:05:43 +0530

    modified

--- 2ndfeb/lab8task.py
+++ 2ndfeb/lab8task.py
@@ -1,8 +1,8 @@
 # Take a user input name,age,rollno,phone no. and print the use details in console
 
 
-name = input("your name is")
-age = input("your age is")
-rollno = input("your rollno is")
-phoneno = input("your phoneno is")
-print("As per the entered details following are your inputs", (name, age, rollno, phoneno))
+name = input("your name is\n")
+age = input("your age is\n")
+rollno = input("your rollno is\n")
+phoneno = input("your phoneno is\n")
+print("As per the entered details following are your inputs\n", '\nName=', name, '\nAge=', age, '\nRollNo.=', rollno, '\nPhoneno=', phoneno)
