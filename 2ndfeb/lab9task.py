5950330b39f2bac7a0f7998c79385e8c2b64e5e8
SRT <sranjant4@gmail.com>
2024-02-08 22:39:46 +0530

    modified

--- 2ndfeb/lab9task.py
+++ 2ndfeb/lab9task.py
@@ -1,5 +1,5 @@
 # Print the Table of 2 by using the command print
-
+# way 1
 A = 2
 print(A, '*', 1, '=', (A*1))
 print(A, '*', 2, '=', (A*2))
@@ -12,3 +12,15 @@ print(A, '*', 8, '=', (A*8))
 print(A, '*', 9, '=', (A*9))
 print(A, '*', 10, '=', (A*10))
 
+#way2
+A=70
+print(f'{A}*1=',(A*1))
+print(f'{A}*2=',(A*2))
+print(f'{A}*3=',(A*3))
+print(f'{A}*4=',(A*4))
+print(f'{A}*5=',(A*5))
+print(f'{A}*6=',(A*6))
+print(f'{A}*7=',(A*7))
+print(f'{A}*8=',(A*8))
+print(f'{A}*9=',(A*9))
+print(f'{A}*10=',(A*10))
