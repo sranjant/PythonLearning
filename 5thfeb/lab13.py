0922b5dafb242b637961d92d8f0a292f029a3e85
sranjant <sranjant4@gmail.com>
2024-02-11 11:06:03 +0530

    dding new code for lab 13

---
+++ 5thfeb/lab13.py
@@ -0,0 +1,13 @@
+Name='Pintu'
+# length of name:-lenth always starts from 1)
+print(len(Name))
+# index always starts from 0
+print(Name[3])
+
+# combine both length and index
+
+print( Name[len(Name)-1 ] )
+
+# Reverse indexation in python
+
+print(Name[-6])
