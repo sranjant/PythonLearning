cc1aff543bb2db4e1757e77bdf4f86c46137a627
sranjant <sranjant4@gmail.com>
2024-02-12 00:49:33 +0530

    modified

--- 7thfeb/lab20Task.py
+++ 7thfeb/lab20Task.py
@@ -1,6 +1,4 @@
-import math
-a = float(input("enter a number\n"))
-sqr = math.sqrt(a)
+import :sqr = math.sqrt(a)
 cube = math.pow(a, 1/3)
 print("squre of entered number is:" ,sqr)
 print("cube of entered number is:" ,cube)
