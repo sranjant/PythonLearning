78b9db332ad2194ff2a0f876e46532575b942961
sranjant <sranjant4@gmail.com>
2024-02-12 00:43:41 +0530

    modified

--- 7thfeb/lab20Task.py
+++ 7thfeb/lab20Task.py
@@ -1,6 +1,6 @@
 import math
 a = int(input("enter a number\n"))
-sqr = pow(a, 2)
-cube = pow(a, 3)
+sqr = math.sqrt(a)
+cube = math.pow(a, 1/3)
 print("squre of entered number is:" ,sqr)
 print("cube of entered number is:" ,cube)
